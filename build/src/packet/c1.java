962ed3d892cced0fdb3290e4d2bffae7e05e629a
FARHAN <FARHAN@DESKTOP-DJ3II4P>
2022-07-12 16:30:01 +0530

    code pushed

---
+++ build/src/packet/c1.java
@@ -0,0 +1,14 @@
+package packet;
+
+import org.testng.annotations.Test;
+
+public class c1 
+{
+	@Test
+	public void testcase()
+	{
+		System.out.println("tescase is running");
+	System.out.println("tescase1 is running");
+	System.out.println("tescase2 is running");
+
+}}
